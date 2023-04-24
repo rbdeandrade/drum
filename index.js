3ca25c0e5bce6b610f266c94595d06cc1684bb17
Renato Braga de Andrade <rbdeandrade@gmail.com>
2023-04-24 18:56:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
     makeSound(buttonInnerHTML);
 
+    buttonAnimation(buttonInnerHTML);
+
   });
 }
 
   document.addEventListener("keydown", function (event) {
 
     makeSound(event.key);
+
+    buttonAnimation(event.key);
 
   });
 
@@ -58,4 +62,16 @@ function makeSound (key) {
 
   }
 
+}
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+  
 }
